42e175c403d35975c78c5972ef133250e84cf2ed
Beerus108 <143139887+Beerus108@users.noreply.github.com>
2023-08-24 16:57:50 -0600

    Update idk

--- README.m
+++ README.m
@@ -1,6 +1,5 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
-
-local Window = Rayfield:CreateWindow({
+local MainWindow = Rayfield:CreateWindow({
    Name = "Rayfield Example Window",
    LoadingTitle = "Rayfield Interface Suite",
    LoadingSubtitle = "by Sirius",
